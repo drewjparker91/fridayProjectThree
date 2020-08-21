a42e21ee820a9d4a6bd7940a8819d0ad7236879f
drewparker <drewjparker91@gmail.com>
2020-08-21 10:30:02 -0700

    Simplify function and for statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/bootstrap.css">
-  <link rel="stylesheet" href="css/styles.css">
-  <script src="js/jquery-3.5.1.js"></script>
-  <script src="js/scripts.js" ></script>
-  <title>Mr. Roboger's Neighborhood</title>
-</head>
-<body>
-  <div class = "jumbotron">Mr. ROBOger's Neighborhood!</div>
+$(document).ready(function() {
+  $("#numberForm").submit(function(event) {
+  event.preventDefault();
+  let end = $("#inputnumber").val();
+  let 
+  
+
+  }
+
+
+
+
+  function submitArray (end) {
+    let numbers = [];
+    for (let i = 0; i < origNumber; i++) {
+      let sentence = index.toString();
+      if(sentence.includes(3)) {
+      numbers.push("Won't you be my neighbor?");
+      } else if (words.includes(2)) {
+        numbers.push("Boop!");
+      } else if (words.includes(1)) {
+        numbers.push("Beep!")
+      } else {
+        numbers.push(sentence);
+      }
+
+    } 
+    return numbers;
+  }
+});
