17c699f0a0bae9b6eb3a5727de44cffe167dccab
drewparker <drewjparker91@gmail.com>
2020-08-21 11:28:34 -0700

    Add pseudocode to convert user inputted integer into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,37 @@
 $(document).ready(function() {
-  $("#numberForm").submit(function(event) {
+  $("#numberForm").submit(function() {
   event.preventDefault();
-  let end = $("#inputnumber").val();
-  let 
   
 
+  
   }
+}
 
 
 
 
-  function submitArray (end) {
-    let numbers = [];
-    for (let i = 0; i < origNumber; i++) {
-      let sentence = index.toString();
-      if(sentence.includes(3)) {
-      numbers.push("Won't you be my neighbor?");
-      } else if (words.includes(2)) {
-        numbers.push("Boop!");
-      } else if (words.includes(1)) {
-        numbers.push("Beep!")
-      } else {
-        numbers.push(sentence);
-      }
+ 
 
-    } 
-    return numbers;
-  }
-});
+
+
+GOAL: convert userNumber into an array from 0-userNumber
+
+
+GOAL: replace 1 with beep! | replace 2 with boop! | replace 3 with "wont you be my neighbor"
+
+const userArray = [1,2,3,4,5]
+let outputArray = []
+for (let i=0; index <= userArray.length; i++)
+  if a number in userArray .includes(3)
+  .push "won't you be my neighbor" to outputArray
+  else if a number in userArray .includes(2)
+  .push "boop!" to outputArray
+  else if a number in userArray .includes(1)
+  .push "beep!" to outputArray
+  else if the number contains none of the numbers, push back original number in UserArray
+
+
+  
+
+
+
