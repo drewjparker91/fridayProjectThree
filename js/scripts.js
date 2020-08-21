7f2465200185ef1bf4675b823f081e221104de11
drewparker <drewjparker91@gmail.com>
2020-08-21 13:07:25 -0700

    Add working user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
   $("#numberForm").submit(function() {
   event.preventDefault();
-  
+    // Get user input
+    let userNum = parseInt($("#inputNumber").val());
 
-  
-  }
-}
+
+
+    // Looping Logic
+    let outputArray = []
+    for (let i=0; i <= userNum; i++) {
+      console.log('>>>' + i);
+      outputArray.push(i);
+    }
+
+    // Display Results
+    $("#outputText").text(outputArray);
+    $("#outputText").show();
+  })
+})
 
 
 
@@ -14,30 +26,32 @@ $(document).ready(function() {
 
 
 
-GOAL: convert userNumber into an array from 0-userNumber
+// GOAL: convert userNumber into an array from 0-userNumber
 
 
-GOAL: replace 1 with beep! | replace 2 with boop! | replace 3 with "wont you be my neighbor"
+// GOAL: replace 1 with beep! | replace 2 with boop! | replace 3 with "wont you be my neighbor"
 
-const userArray = [1,2,3,4,5]
-let outputArray = []
-for (let i=0; index <= userArray.length; i++)
-  if a number in userArray .includes(3)
-  .push "won't you be my neighbor" to outputArray
-  else if a number in userArray .includes(2)
-  .push "boop!" to outputArray
-  else if a number in userArray .includes(1)
-  .push "beep!" to outputArray
-  else if the number contains none of the numbers, push back original number in UserArray
+// const userNum = 7
+// let outputArray = []
+// for (let i=0; index <= userNum; i++) {
+//   console.log('>>>' + i)
 
-
-  GOAL: create error messages
-
-  if userNumber is Nan 
-  else if user number is empty
-  return "Whoops! Thats not a number! Please enter a number!"
-  else usernumber is over 1000
-  return "Hey neighbor, enter a smaller number, i can't count past 1000!"
+//}
+  // if a number in usernum.length .includes(3)
+  // .push "won't you be my neighbor" to outputArray
+  // else if a number in userArray .includes(2)
+  // .push "boop!" to outputArray
+  // else if a number in userArray .includes(1)
+  // .push "beep!" to outputArray
+  // else if the number contains none of the numbers, push back original number in UserArray
 
 
 
+
+  // GOAL: create error messages
+
+  // if userNumber is Nan 
+  // else if user number is empty
+  // return "Whoops! Thats not a number! Please enter a number!"
+  // else usernumber is over 1000
+  // return "Mr Roboto can only count to 10000!"
